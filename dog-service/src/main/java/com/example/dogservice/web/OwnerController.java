4309fa1ec735d33a6307f20731d750a092938260
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2026-03-27 16:14:51 -0700

    Log error and signal it

--- dog-service/src/main/java/com/example/dogservice/web/OwnerController.java
+++ dog-service/src/main/java/com/example/dogservice/web/OwnerController.java
@@ -1,13 +1,18 @@
 package com.example.dogservice.web;
 
+
 import com.example.dogservice.service.NoSuchDogOwnerException;
 import com.example.dogservice.service.OwnerService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.ServerHttpObservationFilter;
 
 import java.util.List;
 
@@ -16,6 +21,8 @@ import java.util.List;
  */
 @RestController
 public class OwnerController {
+
+	private final static Logger logger = LoggerFactory.getLogger(OwnerController.class);
 
 	private final OwnerService ownerService;
 
@@ -29,7 +36,10 @@ public class OwnerController {
 	}
 
 	@ExceptionHandler
-	ProblemDetail onNoSuchDogOwner(NoSuchDogOwnerException ex) {
+	ProblemDetail onNoSuchDogOwner(HttpServletRequest request, NoSuchDogOwnerException ex) {
+		logger.error("Ooops!", ex);
+		ServerHttpObservationFilter.findObservationContext(request)
+			.ifPresent(context -> context.setError(ex));
 		ProblemDetail details = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
 		details.setProperty("owner", ex.getName());
 		return details;
